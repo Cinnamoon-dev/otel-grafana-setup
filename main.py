a0c7f82d258150c44f889f1264974490eab9c343
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-05-24 16:58:44 -0300

    feat: creating a base manual instrumentation endpoint

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ app = FastAPI()
 
 @app.get("/rolldice")
 def roll_dice(player: str = None):
-    # TODO
-    # Salvar valor do dado num atributo do span
-
     if player:
         return f"{player} rolled the dice! Result: {randint(1, 6)}"
     return f"Result: {randint(1, 6)}"
 
+@app.get("/manual_rolldice")
+def manual_rolldice(player: str = None):
+    # TODO
+    # Criar meter e tracer para salvar as métricas
+    # Salvar valor do dado num atributo do span
+    if player:
+        return f"{player} rolled the dice! Result: {randint(1, 6)}"
+    return f"Result: {randint(1, 6)}"
+    
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=False)
