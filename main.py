6484f1618612046ab5556e33aca4bfd2276d40b9
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-05-27 12:38:08 -0300

    docs: adding future trace and metrics features

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ def manual_rolldice(player: str = None):
     # TODO
     # Criar meter e tracer para salvar as métricas
     # Salvar valor do dado num atributo do span
+    # Criar um counter para salvar a quantidade de cada valor da rodada do dado numa métricas (quantos ums quantos dois quantos cincos)
+    # Adicionar os atributos player name e dice value ao trace
     if player:
         return f"{player} rolled the dice! Result: {randint(1, 6)}"
     return f"Result: {randint(1, 6)}"
