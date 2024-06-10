a8a32f73a3f49a7f531a4883524c9ae8ce339561
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-06-10 15:55:09 -0300

    refactor:  using prometheus syntax and adding unit to counter

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from fastapi import FastAPI
 from opentelemetry import trace, metrics
 
 tracer = trace.get_tracer("diceroller.tracer")
-meter = metrics.get_meter("diceroller.meter")
+meter = metrics.get_meter("diceroller_meter")
 
-roll_counter = meter.create_counter("dice.rolls", description="The number of rolls by roll value")
+roll_counter = meter.create_counter("dice_rolls", unit="rolls", description="The number of rolls by roll value")
 
 app = FastAPI()
 
@@ -18,12 +18,12 @@ def roll_dice(player: str = None):
 
 @app.get("/manual_rolldice")
 def manual_rolldice(player: str = None):
-    with trace.get_current_span("roll") as roll_span:
+    with tracer.start_as_current_span("roll") as roll_span:
         result = randint(1,  6)
 
         roll_span.set_attribute("player", player)
         roll_span.set_attribute("roll_result", result)
-        roll_counter.add(1, {"roll.value": result})
+        roll_counter.add(1, {"roll_value": result})
 
         if player:
             return f"{player} rolled the dice! Result: {result}"
