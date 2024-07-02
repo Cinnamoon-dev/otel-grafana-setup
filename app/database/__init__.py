42c39134441cea118781c25a5800c494b0eeda9a
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-07-02 19:42:57 -0300

    feat: creating db connection

---
+++ app/database/__init__.py
@@ -0,0 +1,24 @@
+import os
+from fastapi import Depends
+from typing import Annotated
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+
+load_dotenv()
+
+engine = create_engine(os.getenv("DATABASE_URI", None))
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    def _get_db():
+        db = SessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+
+    return next(_get_db())
+
+db_dependency = Annotated[Session, Depends(get_db)]
