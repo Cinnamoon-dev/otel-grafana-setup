65bcf4ac33521240cd95bdd25d2c2ee8ecf4c2d9
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-07-08 19:08:07 -0300

    refactor: importing routers and injecting envs

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-import uvicorn, logging
+import os, uvicorn, logging
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from app.database import Base, engine
 from opentelemetry.sdk.resources import Resource
 from opentelemetry._logs import set_logger_provider
-from app.controllers import instrumentationController
+from app.controllers import instrumentationController, employeeController, vehicleController
 from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
+
+load_dotenv()
 
 # ----------------------------------------------------------------------------------------
 # Configuração de export de logs para o OTEL Collector
@@ -40,6 +45,11 @@ logging.getLogger().addHandler(handler)
 app = FastAPI()
 
 app.include_router(instrumentationController.router)
+app.include_router(employeeController.router)
+app.include_router(vehicleController.router)
+
+FastAPIInstrumentor.instrument_app(app)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, reload=False, host="0.0.0.0")
+    Base.metadata.create_all(bind=engine)
+    uvicorn.run("main:app", port=os.getenv('PORT', 8000), reload=False, host="0.0.0.0")
