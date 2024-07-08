791114bb16dde662a0a10708def5482439325ae2
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-07-08 19:07:30 -0300

    feat: creating employee controller

---
+++ app/adapters/employeeAdapter.py
@@ -0,0 +1,68 @@
+import json
+from fastapi import Response
+from app import instance_update
+from sqlalchemy.orm import Session
+from app.models.EmployeeModel import Employee
+from app.swagger_models.employeeModels import EmployeeRequest, EmployeeEditRequest
+
+class EmployeeAdapter():
+    def list_all_employees(self, db: Session):
+        employees = db.query(Employee).all()
+
+        return {"data": [employee.relationship_to_dict() for employee in employees]}
+
+    def view_one_employee(self, db: Session):
+        employee = db.query(Employee).get(id)
+
+        if not employee:
+            return Response(json.dumps({"error": True, "message": "Employee not found"}), 404) 
+
+        return employee.to_dict()
+
+    def create_one_employee(self, request: dict[EmployeeRequest], db: Session):
+        email = request.get("email").lower()
+
+        if db.query(Employee).filter(Employee.email == email).first():
+            return Response(json.dumps({"error": True, "message": "email already exists"}), 409)
+        
+        newEmployee = Employee(**request)
+        db.add(newEmployee)
+
+        try:
+            db.flush()
+            db.commit()
+            return Response(json.dumps({"error": False, "message": "Employee created successfully"}), 200)
+        except:
+            db.rollback()
+            return Response(json.dumps({"error": True, "message": "Database error"}), 500)
+
+    def edit_one_employee(self, id: int, request: dict[EmployeeEditRequest], db: Session):
+        employee = db.query(Employee).get(id)
+
+        if not employee:
+            return Response(json.dumps({"error": True, "message": "Employee not found"}), 404)
+
+        instance_update(employee, request)
+
+        try:
+            db.commit()
+            return Response(json.dumps({"error": False, "message": "Employee edited successfully"}))
+        except:
+            db.rollback()
+            return Response(json.dumps({"error": True, "message": "Database Error"}, 500))
+
+    def delete_one_employee(self, id: int, db: Session):
+        employee = db.query(Employee).get(id)
+
+        if not employee:
+            return Response(json.dumps({"error": True, "message": "Employee not found"}), 404)
+        
+        try:
+            db.delete(employee)
+            db.flush()
+            db.commit()
+            return Response(json.dumps({"error": False, "message": "Employee deleted successfully"}))
+            
+        except:
+            db.rollback()
+            return Response(json.dumps({"error": True, "message": "Database error"}), 500)
---
+++ app/controllers/employeeController.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+from app.database import db_dependency
+from app.adapters.employeeAdapter import EmployeeAdapter
+from app.swagger_models.employeeModels import EmployeeEditRequest, EmployeeRequest
+
+router = APIRouter(prefix="/employee")
+
+@router.get("/all")
+def get_all_employee(db: db_dependency):
+    return EmployeeAdapter().list_all_employees(db)
+
+@router.get("/view/{id:int}")
+def get_one_employee(id: int, db: db_dependency):
+    return EmployeeAdapter().view_one_employee(id, db)
+
+@router.post("/add")
+def create_one_employee(employee: EmployeeRequest, db: db_dependency):
+    return EmployeeAdapter().create_one_employee(employee, db)
+
+@router.put("/edit/{id:int}")
+def edit_one_employee(id: int, fields: EmployeeEditRequest, db: db_dependency):
+    return EmployeeAdapter().edit_one_employee(id, fields, db)
+
+@router.delete("/delete/{id:int}")
+def delete_one_employee(id: int, db: db_dependency):
+    return EmployeeAdapter().delete_one_employee(id, db)
