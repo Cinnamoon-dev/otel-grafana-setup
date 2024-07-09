7eed5c1aefadd61c6bb6e7bb25186332a2be1f72
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-07-09 16:08:13 -0300

    fix: adding db parameter to adapters and model dump to add controller

--- app/controllers/vehicleController.py
+++ app/controllers/vehicleController.py
@@ -7,20 +7,20 @@ router = APIRouter(prefix="/vehicle")
 
 @router.get("/all")
 def get_all_vehicles(db: db_dependency):
-    return VehicleAdapter().list_all_vehicles()
+    return VehicleAdapter().list_all_vehicles(db)
 
 @router.get("/view/{id:int}")
 def get_one_vehicle(id: int, db: db_dependency):
-    return VehicleAdapter().view_one_vehicle(id)
+    return VehicleAdapter().view_one_vehicle(id, db)
 
 @router.post("/add")
 def create_one_vehicle(vehicle: VehicleRequest, db: db_dependency):
-    return VehicleAdapter().create_one_vehicle(vehicle)
+    return VehicleAdapter().create_one_vehicle(vehicle.model_dump(), db)
 
 @router.put("/edit/{id:int}")
 def edit_one_vehicle(id: int, fields: VehicleEditRequest, db: db_dependency):
-    return VehicleAdapter().edit_one_vehicle(id, fields)
+    return VehicleAdapter().edit_one_vehicle(id, fields, db)
 
 @router.delete("/delete/{id:int}")
 def delete_one_vehicle(id: int, db: db_dependency):
-    return VehicleAdapter().delete_one_vehicle(id)
+    return VehicleAdapter().delete_one_vehicle(id, db)
